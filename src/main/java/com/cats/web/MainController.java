e3742dfe73685b497f2940c26cd5e672ab375bbf
andrey <gorbandrey2@gmail.com>
2017-02-07 21:42:23 +0200

    added base controller

---
+++ src/main/java/com/cats/web/MainController.java
@@ -0,0 +1,19 @@
+package com.cats.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.enterprise.inject.Model;
+
+/**
+ * Created by andrey on 07.02.17.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/main")
+    public String main(Model model){
+        return "main";
+    }
+}
+
