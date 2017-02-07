8135c3ef292d874377eda95207c59794aa59fe6a
andrey <gorbandrey2@gmail.com>
2017-02-07 22:55:58 +0200

    added services

---
+++ src/main/java/com/cats/services/CatService.java
@@ -0,0 +1,23 @@
+package com.cats.services;
+
+import com.cats.model.Cat;
+
+import java.util.Collection;
+
+/**
+ * Created by andrey on 07.02.17.
+ */
+public interface CatService {
+
+    Cat save(Cat cat);
+
+    Cat update(Cat cat);
+
+    Collection<Cat> saveAll(Collection<Cat> cats);
+
+    void delete(Integer catId);
+
+    Cat get(Integer catId);
+
+    Collection<Cat> getAll();
+}
---
+++ src/main/java/com/cats/services/CatServiceImpl.java
@@ -0,0 +1,51 @@
+package com.cats.services;
+
+import com.cats.dao.Dao;
+import com.cats.model.Cat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+
+/**
+ * Created by andrey on 07.02.17.
+ */
+@Service("catService")
+@Transactional
+public class CatServiceImpl implements CatService{
+
+    @Autowired
+    private Dao dao;
+
+    @Override
+    public Cat save(Cat cat) {
+        return dao.save(cat);
+    }
+
+    @Override
+    public Cat update(Cat cat) {
+        return dao.update(cat);
+    }
+
+    @Override
+    public Collection<Cat> saveAll(Collection<Cat> cats) {
+        return dao.saveAll(cats);
+    }
+
+    @Override
+    public void delete(Integer catId) {
+        final Cat cat = dao.get(Cat.class, catId);
+        dao.delete(cat);
+    }
+
+    @Override
+    public Cat get(Integer catId) {
+        return dao.get(Cat.class, catId);
+    }
+
+    @Override
+    public Collection<Cat> getAll() {
+        return dao.all(Cat.class);
+    }
+}
