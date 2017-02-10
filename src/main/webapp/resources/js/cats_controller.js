7d2ec53f3b5438f3b0c118bfcb12c888f79adc1a
andrey <gorbandrey2@gmail.com>
2017-02-10 11:57:20 +0200

    added service and controller for front-end (angularjs)

---
+++ src/main/webapp/resources/js/cats_controller.js
@@ -0,0 +1,104 @@
+'use strict';
+
+angular.module('myApp').controller('CatController', ['$scope', 'CatService', function ($scope, CatService) {
+    var self = this;
+    self.cat = {id: null, age: null, color: '', breed: '', name: '', weight: null};
+    self.cats = [];
+    self.submit = submit;
+    self.edit = edit;
+    self.remove = remove;
+    self.reset = reset;
+
+
+    fetchAllCats();
+
+    function fetchAllCats() {
+        CatService.fetchAllCats()
+            .then(
+                function (res) {
+                    self.cats = res;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching cats');
+                }
+            );
+    }
+
+    function fetchCat(id) {
+        CatService.fetchCat(id)
+            .then(
+                function (res) {
+                    self.cat = res;
+                },
+                function (errResponse) {
+                    console.error('Error while fetching cat');
+                }
+            );
+    }
+
+    function createCat(cat) {
+        CatService.createCat(cat)
+            .then(
+                fetchAllCats,
+                function (errResponse) {
+                    console.error('Error while creating cat');
+                }
+            );
+    }
+
+    function updateCat(cat, id) {
+        CatService.updateCat(cat, id)
+            .then(
+                fetchAllCats,
+                function (errResponse) {
+                    console.error('Error while updating cat');
+                }
+            );
+    }
+
+    function deleteCat(id) {
+        CatService.deleteCat(id)
+            .then(
+                fetchAllCats,
+                function (errResponse) {
+                    console.error('Error while deleting cat');
+                }
+            );
+    }
+
+    function submit() {
+        if (self.cat.id === null) {
+            console.log('Saving New Cat', self.cat);
+            createCat(self.cat);
+        } else {
+            updateCat(self.cat, self.cat.id);
+            console.log('Cat updated with id ', self.cat.id);
+        }
+        reset();
+    }
+
+    function edit(id) {
+        console.log('id to be edited', id);
+        for (var i = 0; i < self.cats.length; i++) {
+            if (self.cats[i].id === id) {
+                self.cat = angular.copy(self.cats[i]);
+                break;
+            }
+        }
+    }
+
+    function remove(id) {
+        console.log('id to be deleted', id);
+        if (self.cat.id === id) {//clean form if the user to be deleted is shown there.
+            reset();
+        }
+        deleteCat(id);
+    }
+
+
+    function reset() {
+        self.cat = {id: null, age: null, color: '', breed: '', name: '', weight: null};
+        $scope.myForm.$setPristine(); //reset Form
+    }
+
+}]);
---
+++ src/main/webapp/resources/js/cats_service.js
@@ -0,0 +1,93 @@
+'use strict';
+
+angular.module('myApp').factory('CatService', ['$http', '$q', function ($http, $q) {
+
+    var REST_SERVICE_URI = 'http://localhost:8080/cats/';
+
+    var factory = {
+        fetchAllCats: fetchAllCats,
+        fetchCat: fetchCat,
+        createCat: createCat,
+        updateCat: updateCat,
+        deleteCat: deleteCat
+    };
+
+    return factory;
+
+    function fetchAllCats() {
+        var deferred = $q.defer();
+        $http.get(REST_SERVICE_URI)
+            .then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (errResponse) {
+                    console.error('Error while fetching cats');
+                    deferred.reject(errResponse);
+                }
+            );
+        return deferred.promise;
+    }
+
+    function createCat(cat) {
+        var deferred = $q.defer();
+        $http.post(REST_SERVICE_URI, cat)
+            .then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (errResponse) {
+                    console.error('Error while creating cat');
+                    deferred.reject(errResponse);
+                }
+            );
+        return deferred.promise;
+    }
+
+
+    function updateCat(cat, id) {
+        var deferred = $q.defer();
+        $http.put(REST_SERVICE_URI + id, cat)
+            .then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (errResponse) {
+                    console.error('Error while updating cat');
+                    deferred.reject(errResponse);
+                }
+            );
+        return deferred.promise;
+    }
+
+    function deleteCat(id) {
+        var deferred = $q.defer();
+        $http.delete(REST_SERVICE_URI + id)
+            .then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (errResponse) {
+                    console.error('Error while deleting cat');
+                    deferred.reject(errResponse);
+                }
+            );
+        return deferred.promise;
+    }
+
+    function fetchCat(id) {
+        var deferred = $q.defer();
+        $http.get(REST_SERVICE_URI + id)
+            .then(
+                function (response) {
+                    deferred.resolve(response.data);
+                },
+                function (errResponse) {
+                    console.error('Error while fetching cat');
+                    deferred.reject(errResponse);
+                }
+            );
+        return deferred.promise;
+    }
+
+}]);
