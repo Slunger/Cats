bd179aa5c9422f04a7e5d205e0dbd768025f5411
andrey <gorbandrey2@gmail.com>
2017-02-11 19:06:24 +0200

    _csrf token for http requests

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-var App = angular.module('myApp',[]);
+var App = angular.module('myApp', []);
+
+App.factory('httpRequestInterceptor', function () {
+    return {
+        request: function (config) {
+            var metas = window.document.getElementsByName("_csrf");
+            config.headers['X-CSRF-TOKEN'] = metas[0].value;
+            return config;
+        }
+    };
+});
+
+App.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+});
