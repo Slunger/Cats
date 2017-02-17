3003b942fef916f4fc00e57370d2135d3f2c8a73
andrey <gorbandrey2@gmail.com>
2017-02-17 14:36:57 +0200

    added pages for confirm oauth and error

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -1,17 +1,3 @@
 'use strict';
 
 var App = angular.module('myApp', []);
-
-App.factory('httpRequestInterceptor', function () {
-    return {
-        request: function (config) {
-            var metas = window.document.getElementsByName("_csrf");
-            config.headers['X-CSRF-TOKEN'] = metas[0].value;
-            return config;
-        }
-    };
-});
-
-App.config(function ($httpProvider) {
-    $httpProvider.interceptors.push('httpRequestInterceptor');
-});
